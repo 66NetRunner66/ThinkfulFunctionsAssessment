6ff1c79505fd0ac0f76ff93145203e949b01a285
Miami_Mike <88cryptorunner88@gmail.com>
2021-03-04 13:36:46 -0500

    Update home.js
    added a helper function
    ###formated the code to be prettier

--- public/src/home.js
+++ public/src/home.js
@@ -1,52 +1,8 @@
 // Note: Please do not change the name of the functions. The tests use those names to validate your code.
 
 function getTotalBooksCount (books) {
-  return books.length;
-}
-
-function getTotalAccountsCount (accounts) {
-  return accounts.length;
-}
-
-function getBooksBorrowedCount (books) {
-  return books.filter (book => !book.borrows[0].returned).length;
-}
-function getMostCommonGenres (books) {
-  let genres = {};
-  for (let i = 0; i < books.length; i++) {
-    if (genres[books[i].genre]) {
-      genres[books[i].genre] += 1;
-    } else {
-      genres[books[i].genre] = 1;
-    }
-  }
-
-  let keys = Object.keys (genres);
-  let result = [];
-  keys.forEach (key => {
-    let vari = {name: key, count: genres[key]};
-    result.push (vari);
-  });
-  result.sort ((a, b) => a < b);
-  if (result.length > 5) {
-    result.shift ();
-  }
-  console.log (genres);
-  console.log (result);
-  return result;
-}
-
-function getMostPopularBooks (books) {
-  let returns = [];
-  for (let i = 0; i < books.length; i++) {
-    returns.push ({name: books[i].title, count: books[i].borrows.length});
-  }
-  returns.sort (
-    (a, b) => (a.count > b.count ? -1 : 1 && console.log (a.count, b.count))// Note: Please do not change the name of the functions. The tests use those names to validate your code.
-
-function getTotalBooksCount (books) {
- let accumulator= 0;
-let result = books.reduce((acc, book)=>acc+1,accumulator);
+  let accumulator = 0;
+  let result = books.reduce ((acc, book) => acc + 1, accumulator);
   return result;
 }
 
@@ -73,13 +29,7 @@ function getMostCommonGenres (books) {
     let vari = {name: key, count: genres[key]};
     result.push (vari);
   });
-  result.sort ((a, b) => a < b);
-  if (result.length > 5) {
-    result.shift ();
-  }
-  console.log (genres);
-  console.log (result);
-  return result;
+  return sort5 (result);
 }
 
 function getMostPopularBooks (books) {
@@ -87,17 +37,7 @@ function getMostPopularBooks (books) {
   for (let i = 0; i < books.length; i++) {
     returns.push ({name: books[i].title, count: books[i].borrows.length});
   }
-  returns.sort (
-    (a, b) => (a.count > b.count ? -1 : 1 && console.log (a.count, b.count))
-  );
-  console.log (returns);
-  for (let i = 0; i < returns.length; i++) {
-    if (returns.length > 5) {
-      returns.pop ();
-    }
-  }
-  console.log (returns);
-  return returns;
+  return sort5 (returns);
 }
 
 function getMostPopularAuthors (books, authors) {
@@ -105,59 +45,23 @@ function getMostPopularAuthors (books, authors) {
   for (let i = 0; i < books.length; i++) {
     for (let j = 0; j < authors.length; j++) {
       if (books[i].authorId === authors[j].id) {
-        let firstandlast = authors[j].name.first+" "+authors[j].name.last;
-      //  firstandlast.join (' ');
+        let firstandlast = authors[j].name.first + ' ' + authors[j].name.last;
+
         authorids.push ({name: firstandlast, count: books[i].borrows.length});
       }
     }
-    authorids.sort ((a, b) => (a.count > b.count ? -1 : 1));
-    for (let i = 0; i < authorids.length; i++) {
-      if (authorids.length > 5) {
-        authorids.pop ();
-      }
-    }
   }
-  console.log (authorids); return authorids;
+  return sort5 (authorids);
 }
 
-module.exports = {
-  getTotalBooksCount,
-  getTotalAccountsCount,
-  getBooksBorrowedCount,
-  getMostCommonGenres,
-  getMostPopularBooks,
-  getMostPopularAuthors,
-};
-
-  );
-  console.log (returns);
-  for (let i = 0; i < returns.length; i++) {
-    if (returns.length > 5) {
-      returns.pop ();
-    }
+//added a helper function
+function sort5 (items) {
+  items.sort ((a, b) => (a.count < b.count ? 1 : -1));
+  let array = [];
+  for (let i = 0; i < 5; i++) {
+    array.push (items[i]);
   }
-  console.log (returns);
-  return returns;
-}
-
-function getMostPopularAuthors (books, authors) {
-  let authorids = [];
-  for (let i = 0; i < books.length; i++) {
-    for (let j = 0; j < authors.length; j++) {
-      if (books[i].authorId === authors[j].id) {
-        let firstandlast = authors[j].name.first+" "+authors[j].name.last;
-      //  firstandlast.join (' ');
-        authorids.push ({name: firstandlast, count: books[i].borrows.length});
-      }
-    }
-    authorids.sort ((a, b) => (a.count > b.count ? -1 : 1));
-    for (let i = 0; i < authorids.length; i++) {
-      if (authorids.length > 5) {
-        authorids.pop ();
-      }
-    }
-  }
-  console.log (authorids); return authorids;
+  return array;
 }
 
 module.exports = {
