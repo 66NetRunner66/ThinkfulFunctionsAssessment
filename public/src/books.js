95e84889abce81834ced402b168e4d34103aba60
Miami_Mike <88cryptorunner88@gmail.com>
2021-03-04 13:39:05 -0500

    Update books.js
    formatted for readability

--- public/src/books.js
+++ public/src/books.js
@@ -1,34 +1,34 @@
 // Note: Please do not change the name of the functions. The tests use those names to validate your code.
 
-function findAuthorById(authors, id) {
-return authors.find((author)=>author.id===id);
+function findAuthorById (authors, id) {
+  return authors.find (author => author.id === id);
 }
-function findBookById(books, id) {
-return books.find((book)=>book.id===id);
-}
-
-function partitionBooksByBorrowedStatus(books) {
-let array = [[],[]];
-array[1].push(books.filter((book)=>!book.borrows[0].returned));
-array[0].push(books.filter((book)=>book.borrows[0].returned));
-const array3 = [...array[1],...array[0]];
-return array3;
+function findBookById (books, id) {
+  return books.find (book => book.id === id);
 }
 
-function getBorrowersForBook(book, accounts) {
-let acc = [];
-for(let i=0;i<book.borrows.length;i++){
-acc.push(book.borrows[i]);
+function partitionBooksByBorrowedStatus (books) {
+  let array = [[], []];
+  array[1].push (books.filter (book => !book.borrows[0].returned));
+  array[0].push (books.filter (book => book.borrows[0].returned));
+  const array3 = [...array[1], ...array[0]];
+  return array3;
 }
-if(acc.length>10){
-  acc.length = 10;
-}
-const list = acc.map((borrow) => {
-  const borrower = accounts.find((account) => account.id === borrow.id);
-  borrower["returned"] = borrow.returned;
-  return borrower;
-});
-return list;
+
+function getBorrowersForBook (book, accounts) {
+  let acc = [];
+  for (let i = 0; i < book.borrows.length; i++) {
+    acc.push (book.borrows[i]);
+  }
+  if (acc.length > 10) {
+    acc.length = 10;
+  }
+  const list = acc.map (borrow => {
+    const borrower = accounts.find (account => account.id === borrow.id);
+    borrower['returned'] = borrow.returned;
+    return borrower;
+  });
+  return list;
 }
 //this code below works as well but the test requires
 //use of the map method as above
@@ -46,7 +46,6 @@ return list;
   }
 }return acc;
 }*/
-
 
 module.exports = {
   findAuthorById,
